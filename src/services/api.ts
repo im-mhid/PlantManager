6c976eef7dc62451bc9a801fe7cc5ccdc5e9c233
im-mhid <hmatheus031@gmail.com>
2021-04-25 18:19:51 -0300

    Update api.ts
    Alterando baseURL para o my json server

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://192.168.1.8:3333'
-})
+    baseURL: 'https://my-json-server.typicode.com/im-mhid/PlantManager'
+});
 
 export default api;
